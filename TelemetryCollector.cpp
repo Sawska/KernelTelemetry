50c77f5e44f70a718622b192f06ef8636831e195
Sawska <Splay1494@gmail.com>
2025-11-02 16:12:04 +0200

    added parsing disk

--- TelemetryCollector.cpp
+++ TelemetryCollector.cpp
@@ -1,13 +1,31 @@
 #include "TelemetryCollector.h"
 #include "Metrics.h"
+#include <cctype>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <ostream>
 #include <sstream>
 #include <string>
 #include <thread>
 #include <vector>
 #include <chrono>
+
+struct DiskStatsRaw {
+    long long readsCompleted = 0;
+    long long sectorsRead = 0;
+    long long writesCompleted = 0;
+    long long sectorsWritten = 0;
+    long long ioTimesMs = 0;
+};
+
+struct NetworkStatsRaw {
+    long long rxBytes = 0;
+    long long txBytes = 0;
+    long long rxPackets = 0;
+    long long txPackets = 0;
+};
+
 
 constexpr double SECTOR_SIZE_BYTES = 512.0;
 
@@ -121,12 +139,6 @@ CpuStats TelemetryCollector::parse_cpu() {
     return cpu;
 }
 
-std::vector<DiskStats> TelemetryCollector::parse_diskstats() {
-    std::vector<DiskStats> allDisks;
-
-
-    return allDisks;
-}
 
 std::vector<NetworkStats> TelemetryCollector::parse_net_dev() {
     std::vector<NetworkStats> allInterfaces;
@@ -134,8 +146,101 @@ std::vector<NetworkStats> TelemetryCollector::parse_net_dev() {
     return allInterfaces;
 }
 
+std::map<std::string,DiskStatsRaw> get_disk_raw_stats() {
+    std::map<std::string,DiskStatsRaw> raw_stats;
+    std::string content = readFile("/proc/diskstats");
+    if (content.empty()) return raw_stats;
+
+    std::stringstream ss(content);
+    std::string line;
+
+    while (std::getline(ss,line)) {
+        std::stringstream line_ss(line);
+        int major,minor;
+        std::string deviceName;
+        DiskStatsRaw raw;
 
 
+        if (!(line_ss >> major >> minor >> deviceName)) continue;
+
+        if (std::isdigit(deviceName.back()) && deviceName.rfind("nvme", 0) != 0)
+            continue;
+
+
+        long long dummy,readMerged,writeMerged,dummy2,ioProgress,dummy3;
+
+        if (!(line_ss >> raw.readsCompleted)) continue;
+        
+        if (!(line_ss >> readMerged)) continue;
+        
+        if (!(line_ss >> raw.sectorsRead)) continue;
+        
+        if (!(line_ss >> dummy)) continue;
+        
+        if (!(line_ss >> raw.writesCompleted)) continue;
+        
+        if (!(line_ss >> writeMerged)) continue;
+        
+        if (!(line_ss >> raw.sectorsWritten)) continue;
+        
+        if (!(line_ss >> dummy)) continue;
+        
+        if (!(line_ss >> ioProgress)) continue;
+        
+        if (!(line_ss >> raw.ioTimesMs)) continue;
+
+        raw_stats[deviceName] = raw;
+    }
+
+    return raw_stats;
+    
+}
+
+std::vector<DiskStats> TelemetryCollector::parse_diskstats() {
+    std::vector<DiskStats> allDisks;
+
+    std::map<std::string,DiskStatsRaw> t1 = get_disk_raw_stats();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    std::map<std::string,DiskStatsRaw> t2 = get_disk_raw_stats();
+
+    for(const auto& pair :t2) {
+        const std::string& deviceName = pair.first;
+        const DiskStatsRaw& raw_t2 = pair.second;
+
+        if (t1.count(deviceName)) {
+            const DiskStatsRaw& raw_t1 = t1.at(deviceName);
+
+            long long reads_diff = raw_t2.readsCompleted - raw_t1.readsCompleted;
+            long long writes_diff = raw_t2.writesCompleted - raw_t1.writesCompleted;
+            long long sectors_read_diff = raw_t2.sectorsRead - raw_t1.sectorsRead;
+            long long sectors_written_diff = raw_t2.sectorsWritten - raw_t1.sectorsWritten;
+
+            DiskStats disk;
+
+            disk.deviceName = deviceName;
+
+            disk.readsPerSecond = static_cast<double>(reads_diff);
+            disk.writesPerSecond = static_cast<double>(writes_diff);
+
+            disk.megabytesReadPerSecond = sectors_read_diff * SECTOR_SIZE_BYTES / 1024.0 / 1024.0;
+            disk.megabytesWrittenPerSecond = sectors_written_diff * SECTOR_SIZE_BYTES / 1024.0 / 1024.0;
+
+            allDisks.push_back(disk);
+        }
+    }
+
+    return allDisks;
+}
+
+std::map<std::string,NetworkStatsRaw> get_net_raw_stats() {
+    std::map<std::string,NetworkStatsRaw> raw_stats;
+    std::string content = readFile("/proc/net/dev");
+    if(content.empty()) return raw_stats;
+
+    std::stringstream ss(content);
+}
 
 SystemMetrics TelemetryCollector::getAllStats() {
     SystemMetrics metrics;
